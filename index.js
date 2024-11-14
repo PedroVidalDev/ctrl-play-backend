962ab83369041149e87ded64dbf7df7cfe21b475
Pedro Henrique Vidal dos Santos <113215138+PedroVidalDev@users.noreply.github.com>
2024-11-14 11:12:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.post('/send-email', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3300;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
